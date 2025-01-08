e10f0c5e87196f741553876e98b0a5b4b46e09a3
theed <kareoedwin@gmail.com>
2025-01-08 20:22:58 +0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -237,7 +237,7 @@ return [
         Webkul\Tax\Providers\TaxServiceProvider::class,
         Webkul\Theme\Providers\ThemeServiceProvider::class,
         Webkul\User\Providers\UserServiceProvider::class,
-        Webkul\MPesa\Providers\MPesaServiceProvider::class,
+        // Webkul\MPesa\Providers\MPesaServiceProvider::class,
     ])->toArray(),
 
     /*
