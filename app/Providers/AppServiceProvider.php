43faccb7a170fabb345f82e3b30305aa3e646c9d
theed <kareoedwin@gmail.com>
2025-01-08 20:36:08 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,14 +7,19 @@ use Illuminate\Support\Facades\ParallelTesting;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
+        if (env('APP_ENV') == 'local' || env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
+
         Schema::defaultStringLength(191);
 
         ParallelTesting::setUpTestDatabase(function (string $database, int $token) {
